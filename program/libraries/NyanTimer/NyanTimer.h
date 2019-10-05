16260f9133eeea55afd7158cc0d85048494ce79d
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2019-10-05 15:11:45 +0900

    lib

---
+++ program/libraries/NyanTimer/NyanTimer.h
@@ -0,0 +1,34 @@
+#ifndef _NyanTimer_h
+#define _NyanTimer_h
+#include "Arduino.h"
+#include <MsTimer2.h>
+#include <TimerOne.h>
+#include <Wire.h>
+#include <ST7032.h>
+
+#define BUTTON1 6 //reset
+#define BUTTON2 8 //inspection mode
+#define BUTTON3 7 //lap up
+#define BUTTON4 4 //lap down
+#define BUZZER 5 //buzzer
+#define LEDR 13 //red led
+#define LEDG 12 //green led
+#define PAD1OUT 15
+#define PAD1IN 16
+#define PAD2OUT 14
+#define PAD2IN 17
+
+class NyanTimer {
+  public:
+    void begin();
+    void lightLED(int LED, bool HL);
+    void setLCD(int row, int col, String str);
+  private:
+    int _LED;
+    bool _HL;
+    int _row;
+    int _col;
+    String _str;
+};
+
+#endif
