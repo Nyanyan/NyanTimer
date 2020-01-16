1be263f67a85b2d2a2d49015d2b42ba735ec706b
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-01-16 14:43:54 +0900

    modified

--- NyanTimer2/NyanTimer2.py
+++ NyanTimer2/NyanTimer2.py
@@ -4,16 +4,59 @@ import RPi.GPIO as GPIO
 root= tk.Tk()
 root.geometry('320x240')
 
-ao5 = tk.StringVar(master=root,value="Ao5: 0.000")
+ao5 = tk.StringVar(master=root,value="Ao5")
 ao5label = tk.Label(root, textvariable=ao5)
 ao5label.grid(row=0, column=0, padx=5, pady=5)
 
-time = tk.StringVar(master=root,value="Time: 0.000")
+time = tk.StringVar(master=root,value="Time")
 timelabel = tk.Label(root, textvariable=time)
 timelabel.grid(row=0, column=1, padx=5, pady=5)
 
-ao12 = tk.StringVar(master=root,value="Ao12: 0.000")
+ao12 = tk.StringVar(master=root,value="Ao12")
 ao12label = tk.Label(root, textvariable=ao12)
 ao12label.grid(row=0, column=2, padx=5, pady=5)
+
+
+ao5num = tk.StringVar(master=root,value="-.---")
+ao5numlabel = tk.Label(root, textvariable=ao5num)
+ao5numlabel.grid(row=1, column=0, padx=5, pady=5)
+
+timenum = tk.StringVar(master=root,value="-.---")
+timenumlabel = tk.Label(root, textvariable=timenum)
+timenumlabel.grid(row=1, column=1, padx=5, pady=5)
+
+ao12num = tk.StringVar(master=root,value="-.---")
+ao12numlabel = tk.Label(root, textvariable=ao12num)
+ao12numlabel.grid(row=1, column=2, padx=5, pady=5)
+
+
+
+bestao5 = tk.StringVar(master=root,value="Best Ao5")
+bestao5label = tk.Label(root, textvariable=bestao5)
+bestao5label.grid(row=0, column=0, padx=5, pady=5)
+
+besttime = tk.StringVar(master=root,value="Best Time")
+besttimelabel = tk.Label(root, textvariable=besttime)
+besttimelabel.grid(row=0, column=1, padx=5, pady=5)
+
+bestao12 = tk.StringVar(master=root,value="Best Ao12")
+bestao12label = tk.Label(root, textvariable=bestao12)
+bestao12label.grid(row=0, column=2, padx=5, pady=5)
+
+
+bestao5num = tk.StringVar(master=root,value="-.---")
+bestao5numlabel = tk.Label(root, textvariable=bestao5num)
+bestao5numlabel.grid(row=1, column=0, padx=5, pady=5)
+
+besttimenum = tk.StringVar(master=root,value="-.---")
+bestbesttimenumlabel = tk.Label(root, textvariable=besttimenum)
+besttimenumlabel.grid(row=1, column=1, padx=5, pady=5)
+
+bestao12num = tk.StringVar(master=root,value="-.---")
+bestbestao12numlabel = tk.Label(root, textvariable=bestao12num)
+bestao12numlabel.grid(row=1, column=2, padx=5, pady=5)
+
+
+
 
 root.mainloop()
