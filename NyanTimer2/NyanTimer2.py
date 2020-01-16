8d9190e70439bfe102a5bc909bec9ab066996a8e
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-01-16 15:27:40 +0900

    modified

--- NyanTimer2/NyanTimer2.py
+++ NyanTimer2/NyanTimer2.py
@@ -1,20 +1,30 @@
 import tkinter as tk
 import RPi.GPIO as GPIO
+import random
 
-def changesession:
+def changesession():
     return 0
 
 root= tk.Tk()
 root.geometry('320x240')
 
-scramble = ""
-scramblevar = tk.StringVar(master=root, value=scramble)
-
 sessions = ['3x3', '2x2', '4x4', '5x5', '6x6', '7x7', '3BLD', '3OH', 'Clock', 'Megaminx', 'Pyraminx', 'Skewb', 'Square-1', '4BLD', '5BLD']
-scramblenums = [25, ]
+scramblenums = [25]
 session = 0
 
-sessionbutton = tk.Button(root, text='Change Session', command=changesession)
+scramble = ""
+rotation = 0
+rotations = [['R', 'L', 'U', 'D', 'F', 'B'], ['R', 'L', 'U', 'D', 'F', 'B', 'Rw', 'Lw', 'Uw', 'Dw', 'Fw', 'Bw']]
+adds = ['', '\'', '2']
+for i in range(scramblenums[session]):
+    rot = rotations[rotation][random.randint(0, len(rotations[rotation]) - 1)]
+    add = adds[random.randint(0, 2)]
+    scramble += rot + add + ' '
+print(scramble)
+
+scramblevar = tk.StringVar(master=root, value=scramble)
+
+sessionbutton = tk.Button(root, text='Session', command=changesession)
 sessionbutton.grid(row=0, column=0, padx=5, pady=5)
 
 sessionvar = tk.StringVar(master=root,value=sessions[session])
@@ -46,35 +56,9 @@ ao12num = tk.StringVar(master=root,value="-.---")
 ao12numlabel = tk.Label(root, textvariable=ao12num)
 ao12numlabel.grid(row=2, column=2, padx=5, pady=5)
 
-
-'''
-bestao5 = tk.StringVar(master=root,value="Best Ao5")
-bestao5label = tk.Label(root, textvariable=bestao5)
-bestao5label.grid(row=2, column=0, padx=5, pady=5)
-
-besttime = tk.StringVar(master=root,value="Best Time")
-besttimelabel = tk.Label(root, textvariable=besttime)
-besttimelabel.grid(row=2, column=1, padx=5, pady=5)
-
-bestao12 = tk.StringVar(master=root,value="Best Ao12")
-bestao12label = tk.Label(root, textvariable=bestao12)
-bestao12label.grid(row=2, column=2, padx=5, pady=5)
-
-
-bestao5num = tk.StringVar(master=root,value="-.---")
-bestao5numlabel = tk.Label(root, textvariable=bestao5num)
-bestao5numlabel.grid(row=3, column=0, padx=5, pady=5)
-
-besttimenum = tk.StringVar(master=root,value="-.---")
-besttimenumlabel = tk.Label(root, textvariable=besttimenum)
-besttimenumlabel.grid(row=3, column=1, padx=5, pady=5)
-
-bestao12num = tk.StringVar(master=root,value="-.---")
-bestao12numlabel = tk.Label(root, textvariable=bestao12num)
-bestao12numlabel.grid(row=3, column=2, padx=5, pady=5)
-'''
-root.grid_columnconfigure(1, weight=1)
-#root.grid_rowconfigure(1, weight=1)
+root.columnconfigure(0, weight=1, uniform='group1')
+root.columnconfigure(1, weight=1, uniform='group1')
+root.columnconfigure(2, weight=1, uniform='group1')
 
 
 root.mainloop()
