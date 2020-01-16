a9df4d149357b96ca09a361f94f7fed6ff4696a9
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-01-16 14:27:37 +0900

    modified

--- NyanTimer2/NyanTimer2.py
+++ NyanTimer2/NyanTimer2.py
@@ -3,4 +3,9 @@ import RPi.GPIO as GPIO
 
 root= tk.Tk()
 root.geometry('320x240')
+
+time = tk.DoubleVar(master=root,value=0.000)
+timelabel = tk.Label(root, textvariable=videolabelvar)
+time.grid(row=3, column=3, padx=5, pady=5)
+
 root.mainloop()
