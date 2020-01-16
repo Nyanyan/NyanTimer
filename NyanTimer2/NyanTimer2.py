81d47e564a8628fdcac0acb9021bc515e0a77e85
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2020-01-16 14:45:46 +0900

    modified

--- NyanTimer2/NyanTimer2.py
+++ NyanTimer2/NyanTimer2.py
@@ -33,28 +33,28 @@ ao12numlabel.grid(row=1, column=2, padx=5, pady=5)
 
 bestao5 = tk.StringVar(master=root,value="Best Ao5")
 bestao5label = tk.Label(root, textvariable=bestao5)
-bestao5label.grid(row=0, column=0, padx=5, pady=5)
+bestao5label.grid(row=2, column=0, padx=5, pady=5)
 
 besttime = tk.StringVar(master=root,value="Best Time")
 besttimelabel = tk.Label(root, textvariable=besttime)
-besttimelabel.grid(row=0, column=1, padx=5, pady=5)
+besttimelabel.grid(row=2, column=1, padx=5, pady=5)
 
 bestao12 = tk.StringVar(master=root,value="Best Ao12")
 bestao12label = tk.Label(root, textvariable=bestao12)
-bestao12label.grid(row=0, column=2, padx=5, pady=5)
+bestao12label.grid(row=2, column=2, padx=5, pady=5)
 
 
 bestao5num = tk.StringVar(master=root,value="-.---")
 bestao5numlabel = tk.Label(root, textvariable=bestao5num)
-bestao5numlabel.grid(row=1, column=0, padx=5, pady=5)
+bestao5numlabel.grid(row=3, column=0, padx=5, pady=5)
 
 besttimenum = tk.StringVar(master=root,value="-.---")
-bestbesttimenumlabel = tk.Label(root, textvariable=besttimenum)
-besttimenumlabel.grid(row=1, column=1, padx=5, pady=5)
+besttimenumlabel = tk.Label(root, textvariable=besttimenum)
+besttimenumlabel.grid(row=3, column=1, padx=5, pady=5)
 
 bestao12num = tk.StringVar(master=root,value="-.---")
-bestbestao12numlabel = tk.Label(root, textvariable=bestao12num)
-bestao12numlabel.grid(row=1, column=2, padx=5, pady=5)
+bestao12numlabel = tk.Label(root, textvariable=bestao12num)
+bestao12numlabel.grid(row=3, column=2, padx=5, pady=5)
 
 
 
